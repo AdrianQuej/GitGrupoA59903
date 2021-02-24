d2deddb565497e8f6082f1dc3f68f29ef1f02037
Adrian Quej <Adrian Quej@LAPTOP-LCPEV7PP>
2021-02-23 20:48:57 -0600

    Primer commit

---
+++ src/gitgrupoa59903/GitGrupoA59903.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitgrupoa59903;
+
+/**
+ *
+ * @author Adrian Quej
+ */
+public class GitGrupoA59903 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Imprimir el nombre del alumno
+        System.out.println("Adrian Ernesto Aguilar Quej");
+    }
+    
+}
